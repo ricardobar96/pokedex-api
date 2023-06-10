cadfdff16ab5d362f5c228805ffa5dff043c35f2
enriqueseor <enriqueseor@gmail.com>
2023-06-10 16:44:28 +0100

    PokemonFetchResults migrated

---
+++ app/src/main/java/cat/teknos/pokedex/api/PokemonFetchResults.kt
@@ -0,0 +1,10 @@
+package cat.teknos.pokedex.api
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class PokemonFetchResults {
+    @SerializedName("results")
+    @Expose
+    val results: ArrayList<*>? = null
+}
